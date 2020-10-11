8a2a5a79d4667466fb6b9ba8a269438485e1b09f
Isatou D Sanyang <isasanyang1986@yahoo.co.uk>
2020-10-10 23:17:00 -0500

    adding files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Rodeo Sandwiches', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Premium Burger', true);
