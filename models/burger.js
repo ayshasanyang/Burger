8a2c2900c16b3180e575dcb631547daad7a5b785
Isatou D Sanyang <isasanyang1986@yahoo.co.uk>
2020-10-10 23:34:04 -0500

    re-arranging the functions

--- models/burger.js
+++ models/burger.js
@@ -3,27 +3,27 @@ var orm = require("../config/orm.js");
 var burger = {
   // READ/SELECT all the records 
 
-  all: function(br) {
-    orm.all('burgers', function(res) {
+  all: function (br) {
+    orm.all('burgers', function (res) {
       br(res);
     });
   },
   // CREATE/INSERT a new record 
-  create: function(cols, vals, br) {
-    orm.create('burgers', cols, vals, function(res) {
+  create: function (cols, vals, br) {
+    orm.create('burgers', cols, vals, function (res) {
       br(res);
     });
   },
   // UPDATE/UPDATE 
 
-  update: function(objColVals, condition, br) {
-    orm.update('burgers', objColVals, condition, function(res) {
+  update: function (objColVals, condition, br) {
+    orm.update('burgers', objColVals, condition, function (res) {
       br(res);
     });
   },
   // DELETE/DELETE a rcord
-  delete: function(condition, br) {
-    orm.delete('burgers', condition, function(res) {
+  delete: function (condition, br) {
+    orm.delete('burgers', condition, function (res) {
       br(res);
     });
   },
